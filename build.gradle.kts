e85d7dff1ec386a587604bda2b8e995d37b91b74
blugon09 <blugon0921@gmail.com>
2023-06-24 01:46:42 +0900

    :sparkles: remove invfx

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,9 @@ java {
     }
 }
 
-val buildPath = File("C:/Files/Minecraft/Servers/Default/plugins")
+val buildPath = File("C:/Files/Minecraft/Servers/Test/plugins")
 val mcVersion = "1.20.1"
 val kotlinVersion = kotlin.coreLibrariesVersion
-
-val invfx = "io.github.monun:invfx-api:3.3.0"
-val kommand = "io.github.monun:kommand-api:3.1.6"
-val tab = "io.github.monun:tap-api:4.9.6"
 
 repositories {
     mavenCentral()
@@ -23,10 +19,11 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
     compileOnly("io.papermc.paper:paper-api:${mcVersion}-R0.1-SNAPSHOT")
-    implementation(invfx)
-    implementation(kommand)
-    implementation(tab)
+//    implementation("io.github.monun:invfx-api:latest.release")
+    implementation("io.github.monun:kommand-api:latest.release")
+    implementation("io.github.monun:tap-api:latest.release")
 }
 
 extra.apply {
@@ -34,9 +31,8 @@ extra.apply {
     set("ProjectVersion", project.version)
     set("KotlinVersion", kotlinVersion)
     set("MinecraftVersion", mcVersion.split(".").subList(0, 2).joinToString("."))
-    set("invfx", invfx.replace("api", "core"))
-    set("kommand", kommand.replace("api", "core"))
-    set("tab", tab.replace("api", "core"))
+    set("kommand", "io.github.monun:kommand-core:3.1.6")
+    set("tap", "io.github.monun:tap-core:4.9.6")
 }
 
 tasks {
