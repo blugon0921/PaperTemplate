076a7b43f8e080bf8bdaef9df9333b685b4f41ed
blugon09 <blugon0921@gmail.com>
2023-07-11 17:32:14 +0900

    ✨ kotlin 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.21"
+    kotlin("jvm") version "1.9.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("io.papermc.paperweight.userdev") version "1.5.3"
 }
